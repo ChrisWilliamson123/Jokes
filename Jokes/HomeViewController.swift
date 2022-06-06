760dc246a277d3cae82f8afe3b0abbabfcd7a6d7
Chris Williamson <cjwilliamson109@gmail.com>
2022-06-06 16:23:00 +0100

    Added a button which prints out some text.

--- Jokes/HomeViewController.swift
+++ Jokes/HomeViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,5 +15,8 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func didTapRandomJokeButton(_ sender: Any) {
+        print("Hello world")
+    }
 }
 
